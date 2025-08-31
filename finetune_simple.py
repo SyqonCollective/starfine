ada920eb3a539ca14216df240f35853ab53bc550
Michael Ruggeri <michaelruggeri@192.168.1.11>
2025-08-31 20:57:19 +0200

    fix: rimozione duplicati/indentazione classe Up, tutto dentro __init__

--- finetune_simple.py
+++ finetune_simple.py
@@ -40,17 +40,6 @@ class Up(nn.Module):
         super().__init__()
         self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
         self.conv = DoubleConv(out_channels * 2, out_channels)
-    def forward(self, x1, x2):
-        x1 = self.up(x1)
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
-        x = torch.cat([x2, x1], dim=1)
-        return self.conv(x)
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-    self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
-    self.conv = DoubleConv(out_channels * 2, out_channels)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
